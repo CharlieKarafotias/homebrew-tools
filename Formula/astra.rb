2f80526c0a12917dfcc02927dbbfdad1de2b4fe7
Charles Karafotias <cnkara2023@gmail.com>
2025-07-22 19:20:18 -0400

    pushing astra formula

---
+++ Formula/astra.rb
@@ -0,0 +1,28 @@
+class Astra < Formula
+    desc "Astra is a powerful daily wallpaper generator that brings stunning, high-quality wallpapers to your desktop"
+    homepage "https://github.com/CharlieKarafotias/astra"
+    url "https://github.com/CharlieKarafotias/astra/archive/refs/tags/v1.0.0.tar.gz"
+    sha256 "a7fd4e3e84f8ebd196407bd11f7720d2a8717afd4fb9f0b92cd95f5a0f919950"
+    license "MIT"
+
+    depends_on "rust" => :build
+
+    def install
+        system "cargo", "build", "--release"
+        bin.install "target/release/astra"
+
+        # Generate and install completions
+        bash_output = Utils.safe_popen_read("#{bin}/astra", "generate-completions", "bash")
+        (bash_completion/"astra").write bash_output
+
+        zsh_output = Utils.safe_popen_read("#{bin}/astra", "generate-completions", "zsh")
+        (zsh_completion/"_astra").write zsh_output
+
+        fish_output = Utils.safe_popen_read("#{bin}/astra", "generate-completions", "fish")
+        (fish_completion/"astra.fish").write fish_output
+    end
+
+    test do
+        system "#{bin}/astra", "--version"
+    end
+end
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# homebrew-tools
+# 🚀 Homebrew Tap: charliekarafotias/tools 🚀
+
+Explore my exciting projects below and take your productivity to the next level!
+
+## ✨ Astra
+
+Supercharge your workflow with Astra! Here's how to install it:
+
+```bash
+brew tap charliekarafotias/tools
+brew install astra
+```
+
+🎉 Astra installed successfully! Unleash its power now!
